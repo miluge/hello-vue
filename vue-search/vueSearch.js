02e1c8942fd8735affbeb82919bd033dbd16b3f3
miluge <gllmblndl@gmail.com>
2020-09-30 14:57:26 +0200

    Updated page
    Added filter for director, year and producer

--- vue-search/vueSearch.js
+++ vue-search/vueSearch.js
@@ -21,8 +21,12 @@ const vm = new Vue({
           }
           return this.items.filter(function(item){
               // https://www.reddit.com/r/vuejs/comments/62kfae/how_do_i_create_very_simple_instant_search_filter/
-              // Must be of string type to work ! 
-            return item.title.toLowerCase().indexOf(self.searchText) >= 0;
+              // Must be of string type 
+            return item.title.toLowerCase().indexOf(self.searchText) >= 0 ||
+                   item.producer.toLowerCase().indexOf(self.searchText) >= 0 ||
+                   item.director.toLowerCase().indexOf(self.searchText) >= 0 ||
+                   item.release_date.toString().indexOf(self.searchText) >= 0;
+
           });
         }
     }
